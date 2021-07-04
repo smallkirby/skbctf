af2fe37aae72f2d8c38b75474fb803a32353b19e
smallkirby <ssmallkirby@gmail.com>
2021-07-04 22:13:44 +0900

    ⚡️ always reload challenges

--- plugins/challfetch.js
+++ plugins/challfetch.js
@@ -3,12 +3,10 @@ import axios from 'axios'
 export default async (context) => {
   const store = context.app.store
   const config = context.app.$config
-  if (store.getters.challs.length === 0) {
-    const { data } = await axios.get(`${config.microCmsApiUrl}/challenges`, {
-      headers: {
-        'X-API-KEY': config.microCmsApiKey,
-      },
-    })
-    store.commit('setChalls', data.contents)
-  }
+  const { data } = await axios.get(`${config.microCmsApiUrl}/challenges`, {
+    headers: {
+      'X-API-KEY': config.microCmsApiKey,
+    },
+  })
+  store.commit('setChalls', data.contents)
 }
