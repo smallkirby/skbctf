5317e1c180bead864b40a953ba1751b07e74734b
smallkirby <ssmallkirby@gmail.com>
2021-11-11 21:04:34 +0900

    🚨 fix lint problem (change rule)

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,5 +15,8 @@ module.exports = {
   },
   plugins: [],
   // add your custom rules here
-  rules: {},
+  rules: {
+    'vue/multi-word-component-names': ['off'],
+    'import/no-named-as-default-member': ['off'],
+  },
 }
